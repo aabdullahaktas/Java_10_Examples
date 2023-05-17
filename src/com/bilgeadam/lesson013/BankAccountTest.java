865d7054745c11e51a560c40c791876429efc493
musty1406 <musty1406@gmail.com>
2023-05-17 12:35:35 +0300

    17/05/2023 gun sonu

---
+++ src/com/bilgeadam/lesson013/BankAccountTest.java
@@ -0,0 +1,28 @@
+package com.bilgeadam.lesson013;
+
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		BankAccount.interestRate = 0.08;
+		BankAccount bankAccount1 = new BankAccount("123", 6989);
+		// bankAccount1.accountNumber = "123";
+		// bankAccount1.balance = 1258.50;
+
+		bankAccount1.getInformation();
+
+		BankAccount bankAccount2 = new BankAccount("1456", 159868);
+		bankAccount2.accountNumber = "456";
+		bankAccount2.balance = 561258.50;
+		bankAccount2.getInformation();
+
+		BankAccount bankAccount3 = new BankAccount("987", 1589);
+		bankAccount3.getInformation();
+
+		BankAccount bankAccount4 = new BankAccount("5689");
+		bankAccount4.getInformation();
+
+		BankAccount bankAccount5 = new BankAccount(500, "11125");
+
+	}
+
+}
