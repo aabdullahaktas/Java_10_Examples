c1f047c28a09400042c764e72edfd565732fc91a
musty1406 <musty1406@gmail.com>
2023-05-30 12:19:28 +0300

    30/05/2023 gun sonu

--- src/com/bilgeadam/lesson019/set/SetOrnek.java
+++ src/com/bilgeadam/lesson019/set/SetOrnek.java
@@ -1,8 +1,8 @@
 package com.bilgeadam.lesson019.set;
 
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
 /*
  * 
@@ -19,14 +19,19 @@ import java.util.Set;
  * 
  */
 public class SetOrnek {
-	Set<String> kategoriler = new HashSet<>();
+	Set<String> kategoriler = new TreeSet<>();
 	Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		SetOrnek setOrnek = new SetOrnek();
-		setOrnek.kategoriEkle2();
-		setOrnek.kategoriEkle2();
-		setOrnek.kategoriEkle2();
+//		setOrnek.kategoriEkle2();
+//		setOrnek.kategoriEkle2();
+//		setOrnek.kategoriEkle2();
+
+		setOrnek.kategoriler.add("Drama");
+		setOrnek.kategoriler.add("Anime");
+		setOrnek.kategoriler.add("Komedi");
+		setOrnek.kategoriler.add("Gerilim");
 
 		for (String string : setOrnek.kategoriler) {
 			System.out.println(string);
---
+++ src/com/bilgeadam/lesson020/KarakterDegistirme.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.lesson020;
+
+/*
+ * bu iki arraydan bir map olusturalım 
+ * ve map uzerinde çalışalım 
+ * dışarıdan girilen kelimen turkce karakteri varsa ingilizce karaktere cevireceğiz
+ * 
+ * 
+ */
+public class KarakterDegistirme {
+	char[] turkishWords = { 'İ', 'ı', 'ş', 'Ş', 'ç', 'Ç', 'ğ', 'Ğ', 'ü', 'Ü', 'ö', 'Ö' };
+	char[] englishWords = { 'I', 'i', 's', 'S', 'c', 'C', 'g', 'G', 'u', 'U', 'o', 'O' };
+}
