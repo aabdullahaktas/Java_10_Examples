a201836461ffd00ae097b1ced868d3dd43162bca
musty1406 <musty1406@gmail.com>
2023-07-03 12:21:13 +0300

    03/07/2023 gun sonu

---
+++ src/com/bilgeadam/lesson032/factory/pizzaApp/KlasikPizza.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.lesson032.factory.pizzaApp;
+
+public class KlasikPizza extends Pizza {
+
+	public KlasikPizza(ETur tur, EHamurTipi hamurTipi, EBoyut boyut) {
+		super(tur, hamurTipi, boyut);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/bilgeadam/lesson032/factory/pizzaApp/VeganPizza.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.lesson032.factory.pizzaApp;
+
+public class VeganPizza extends Pizza {
+
+	public VeganPizza(ETur tur, EHamurTipi hamurTipi, EBoyut boyut) {
+		super(tur, hamurTipi, boyut);
+
+	}
+
+}
